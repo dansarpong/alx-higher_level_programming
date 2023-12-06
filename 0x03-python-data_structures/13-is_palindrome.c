76684b6e7ed745f0766bd380b571b427b74ef574
Daniel Sarpong <dansarpong.me@gmail.com>
2023-12-06 13:53:23 +0000

    Fix task 13
    Efficiency trial

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include "lists.h"
 
-listint_t *create_rev_copy(listint_t *);
-
 /**
   * is_palindrome - checks if a singly linked list is a palindrome
   * @head: pointer to the head of the linked list
@@ -10,50 +8,33 @@ listint_t *create_rev_copy(listint_t *);
   */
 int is_palindrome(listint_t **head)
 {
-	listint_t *rev_copy = NULL, *check_a, *check_b;
+	listint_t *check_a = *head, *check_b = *head, *check_c;
+	int len = 0, hwDeep = 0, j;
 
 	if (head == NULL || *head == NULL)
 		return (1);
 
-	rev_copy = create_rev_copy(*head);
-	if (rev_copy == NULL)
-		return (0);
-
-	check_a = *head;
-	check_b = rev_copy;
-	while (check_a && check_b)
+	while (check_b)
 	{
-		if (check_a->n != check_b->n)
-		{
-			free_listint(rev_copy);
-			return (0);
-		}
-		check_a = check_a->next;
+		len++;
 		check_b = check_b->next;
 	}
 
-	free_listint(rev_copy);
-	return (1);
-}
-
-/**
-  * create_rev_copy - create a copy of the linked list in reverse
-  * @head: pointer to head of the original linked list
-  * Return: pointer to the head of the new copy
-  */
-listint_t *create_rev_copy(listint_t *head)
-{
-	listint_t *new, *copy = NULL;
-
-	if (!head)
-		return (NULL);
-	copy = create_rev_copy(head->next);
-	new = add_nodeint_end(&copy, head->n);
-	if (!new)
+	for (hwDeep = 1; hwDeep <= len / 2; hwDeep++)
 	{
-		free_listint(copy);
-		return (NULL);
+		check_c = check_a->next;
+		check_b = check_a;
+		for (j = 1; check_c; check_c = check_c->next)
+		{
+			if (j != hwDeep)
+				j++;
+			else
+				check_b = check_b->next;
+		}
+		if (check_b->n != check_a->n)
+			return (0);
+		check_a = check_a->next;
 	}
 
-	return (copy);
+	return (1);
 }
