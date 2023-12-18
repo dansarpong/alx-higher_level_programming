6108ae23ac4321deaccb422d3955fb8cbada5ded
dansarpong <dansarpong.me@gmail.com>
2023-12-18 22:13:00 +0000

    Fix task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (ZeroDivisionError, TypeError):
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: {}".format(result))
+
+    return result
