963661c59816114d39adc2c342f8a26014ae48a0
dansarpong <dansarpong.me@gmail.com>
2023-12-19 23:55:32 +0000

    Do task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """an empty class Square that defines a square"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+## 0x06-python-classes
+
+![Image](https://media.makeameme.org/created/be-happy-and-85de421f36.jpg)
