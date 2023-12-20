c2248e4f1200a8310f01d1adf5e709a4f76f0265
dansarpong <dansarpong.me@gmail.com>
2023-12-20 00:09:15 +0000

    Do task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """class Square with private instance attribute: size"""
+    def __init__(self, size):
+        """initializer args: size (int) Return: None"""
+        self.__size = size
