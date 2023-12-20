d0c268aada7287f10090eb961ff7088d597c9247
dansarpong <dansarpong.me@gmail.com>
2023-12-20 00:36:54 +0000

    Do task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """ A Square class with private and optional instantion attribute: size """
+    def __init__(self, size=0):
+        """ initializes the square """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Finds and returns the current square area """
+        return self.__size ** 2
