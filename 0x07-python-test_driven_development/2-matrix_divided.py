d6fe746cada728352cae97261c4f9f3d5c9a86ba
dannycypher <dansarpong.me@gmail.com>
2024-01-08 22:53:34 +0000

    Fix task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,23 +5,28 @@
 def matrix_divided(matrix, div):
     """ Divides all elements of a matrix """
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        err = "matrix must be a matrix (list of lists) of integers/floats"
+        raise TypeError(err)
     if len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        err = "matrix must be a matrix (list of lists) of integers/floats"
+        raise TypeError(err)
     for row in matrix:
         if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            err = "matrix must be a matrix (list of lists) of integers/floats"
+            raise TypeError(err)
         if len(row) == 0:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            err = "matrix must be a matrix (list of lists) of integers/floats"
+            raise TypeError(err)
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if type(element) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+                "matrix must be a matrix (list of lists) of integers/floats"
+                raise TypeError(err)
+
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     return [[round(element / div, 2) for element in row] for row in matrix]
