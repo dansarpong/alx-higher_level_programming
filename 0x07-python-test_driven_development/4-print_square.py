045022a92b67904eada346a14a8e1cbc803365e5
dannycypher <dansarpong.me@gmail.com>
2024-01-08 03:32:53 +0000

    Do task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Defines a function that prints a square with the character # """
+
+
+def print_square(size):
+    """ Prints a square with the character # """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        for i in range(size):
+            for j in range(size):
+                print("#", end="")
+            print()
