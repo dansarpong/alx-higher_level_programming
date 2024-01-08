1de3cfc80d8e27c500f14145a096d20ec47b511d
dannycypher <dansarpong.me@gmail.com>
2024-01-08 22:37:18 +0000

    Fix task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -23,4 +23,4 @@ class Square(Rectangle):
     def __str__(self):
         """ Returns a string representation of a Square instance """
 
-        return "[Square] {}/{}".format(self.__width, self.__height)
+        return "[Square] {}/{}".format(self.__size, self.__size)
