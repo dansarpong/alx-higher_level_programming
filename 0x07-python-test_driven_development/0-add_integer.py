612f69205137fee38c1bb2959c755df536bd38fa
dannycypher <dansarpong.me@gmail.com>
2024-01-08 02:18:51 +0000

    Do task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Defines a function that adds two integers """
+
+
+def add_integer(a, b=98):
+    """ Adds two integers """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
