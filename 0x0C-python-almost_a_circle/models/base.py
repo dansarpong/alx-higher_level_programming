189d7e6074f8ad0a582fff046ae1ab798daa7f3c
dannycypher <dansarpong.me@gmail.com>
2024-01-14 00:18:17 +0000

    Add Base class to models package

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This module defines the Base class for the project """
+
+
+class Base:
+    """ The Base class for the project """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ The initializing function for the Base class """
+
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
