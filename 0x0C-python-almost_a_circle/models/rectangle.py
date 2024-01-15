46aca36e5cca03884c788b33adafcb10253c1e9e
dannycypher <dansarpong.me@gmail.com>
2024-01-15 19:29:38 +0000

    Fix shebang in rectangle.py and square.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """ Module that defines the Rectangle class that inherits from Base """
 from models.base import Base
 
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """ Module that defines the Square class that inherits from Rectangle """
 from models.rectangle import Rectangle
 
