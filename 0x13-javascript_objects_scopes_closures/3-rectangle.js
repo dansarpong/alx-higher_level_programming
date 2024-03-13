04f8eebc70da323b6e4a50d0b084da29adb75d9f
dansarpong <dansarpong.me@gmail.com>
2024-03-13 02:27:36 +0000

    Task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+'use strict';
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let x = 0;
+    let y = 0;
+    let str = '';
+    for (x = 0; x < this.width; x++) {
+      str += 'X';
+    }
+    for (y = 0; y < this.height; y++) {
+      console.log(str);
+    }
+  }
+};
